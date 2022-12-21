ffa4343d51cf6c263f548d2b48a758420e2b9081
danny <dannytokarev@gmail.com>
2022-12-21 11:05:30 -0500

    added functionality to display data on page. basic styling

--- script.js
+++ script.js
@@ -2,20 +2,21 @@ let latitude;
 let longitude;
 let cityName;
 const searchButton = document.querySelector(".searchBtn");
+const outputDisplay = document.querySelector(".output");
+const textBar = document.querySelector(".locationInput");
+const temperatureValue = document.querySelector(".temp");
+const skyView = document.querySelector(".skyView");
+const windSpeed = document.querySelector(".windSpeed");
 
-searchButton.addEventListener("click", () => {});
-
-async function getMeWeatherData(cityName) {
+async function getMeWeatherData() {
 	const response = await fetch(
-		`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=53dabd4298e397b8b0097f65387786a3&units=imperial`,
+		`https://api.openweathermap.org/data/2.5/weather?q=${textBar.value}&appid=53dabd4298e397b8b0097f65387786a3&units=imperial`,
 		{ mode: "cors" }
 	);
 	const weatherData = await response.json();
-	console.log(weatherData.main.temp);
+	temperatureValue.textContent = "Temperature:" + " " + weatherData.main.temp;
+	skyView.textContent = weatherData.weather[0].description;
+	windSpeed.textContent = weatherData.wind.speed + "mph";
 }
 
-getMeWeatherData("london");
-
-function clickMe() {
-	console.log("iwork");
-}
+searchButton.addEventListener("click", getMeWeatherData);
